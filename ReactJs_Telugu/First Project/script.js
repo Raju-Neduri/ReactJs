45b167d4809555c62cf3b87763fc0f7d9a4f4987
Neduri Raju <rajneduri123@gmail.com>
2025-04-11 12:51:59 +0530

    hq and div content changed

--- ReactJs_Telugu/First Project/script.js
+++ ReactJs_Telugu/First Project/script.js
@@ -1,2 +1,25 @@
 console.log("Successfully completed my first project");
-document.getElementById("submitButton")
+
+const changebutton = document.getElementById("submitButton");
+console.log(changebutton);
+
+function greet(){
+  console.log("Hello Raju");
+}
+
+greet();
+
+changebutton.addEventListener('click',function(){
+// console.log("function called now")
+
+//To change H1 tag content
+const titleElement=document.getElementById("title");
+console.log("titleElement");
+titleElement.textContent="Successfully completed the first project";
+
+//To add html content in DIV
+const divElement=document.getElementById("paragraphId");
+console.log("divElement");
+divElement.innerHTML="<p>This is a new pargraph</p>";
+
+});
