15f3b833bba111862c6b28b7693b9783c3921924
Neduri Raju <rajneduri123@gmail.com>
2025-04-13 12:43:12 +0530

    colors appiled

--- ReactJs_Telugu/first_project/src/App.js
+++ ReactJs_Telugu/first_project/src/App.js
@@ -1,34 +1,19 @@
-// import logo from './logo.svg';
+
 import AboutUs from './AboutUs';
 import './App.css';
 import { Member } from './Member';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
+   
     <div>
-      <h1 style={{backgroundColor:'lightblue', textAlign:"center"}}>First batch is the best batch</h1>
+      <h1 style={{backgroundColor:'#3A59D1', textAlign:"center", color:'white'}}>First batch is the best batch</h1>
      <AboutUs />
-
+        <div className="members-container">
           <Member name="Raju" city="Hyderabad"/>
           <Member name="Ram" city="jammikunta"/>
           <Member name="vaishu" city="Hanamkonda" />
+    </div>
     </div>
   
   );
--- ReactJs_Telugu/first_project/src/Member.js
+++ ReactJs_Telugu/first_project/src/Member.js
@@ -1,9 +1,12 @@
+
+import "./Member.css";
 export function Member(props) {
   console.log(props.city);
   const memebersName=props.name;
   const memebersCity=props.city;
+
   return (
-    <div>
+    <div className="memberCard">
       <h2>{memebersName}</h2>
       <p>{memebersCity}</p>
     </div>
