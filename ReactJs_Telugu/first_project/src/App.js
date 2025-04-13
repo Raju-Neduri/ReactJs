e984ded58eea76a3b9fac5f01f03d9047eb5795a
Neduri Raju <rajneduri123@gmail.com>
2025-04-13 13:27:02 +0530

    updated

--- ReactJs_Telugu/first_project/src/App.js
+++ ReactJs_Telugu/first_project/src/App.js
@@ -4,10 +4,46 @@ import './App.css';
 import { Member } from './Member';
 
 function App() {
+
+const appStyle={backgroundColor:'#3A59D1', textAlign:"center", color:'white'}
+
+const people = [
+  { name: "Raju", city: "Jammikunta" },
+  { name: "Vaishu", city: "Houseboard Colony" },
+  { name: "Abhilash", city: "Valbhapur" },
+  { name: "Ram", city: "Hyderabad" },
+  { name: "Anu", city: "Karimnagar" },
+  { name: "Pooja", city: "Warangal" },
+  { name: "Surya", city: "Kazipet" },
+  { name: "Meghana", city: "Hanamkonda" },
+  { name: "Kiran", city: "Chityal" },
+  { name: "Bhavya", city: "Guntur" },
+  { name: "Vamsi", city: "Khammam" },
+  { name: "Sravani", city: "Nalgonda" },
+  { name: "Manoj", city: "Bhimavaram" },
+  { name: "Neha", city: "Vijayawada" },
+  { name: "Akash", city: "Rajahmundry" },
+  { name: "Harsha", city: "Nizamabad" },
+  { name: "Deepthi", city: "Adilabad" },
+  { name: "Sandeep", city: "Karimnagar" },
+  { name: "Lahari", city: "Siddipet" },
+  { name: "Rohit", city: "Mahabubabad" },
+  { name: "Divya", city: "Warangal" },
+  { name: "Tarun", city: "Yellandu" },
+  { name: "Keerthi", city: "Huzurabad" },
+  { name: "Venky", city: "Manthani" },
+  { name: "Madhu", city: "Kothagudem" },
+  { name: "Swapna", city: "Sircilla" },
+  { name: "Praveen", city: "Bhadrachalam" },
+  { name: "Lavanya", city: "Hanamkonda" },
+  { name: "Sushma", city: "Metpally" },
+  { name: "Rakesh", city: "Jagtial" }
+];
+
   return (
    
     <div>
-      <h1 style={{backgroundColor:'#3A59D1', textAlign:"center", color:'white'}}>First batch is the best batch</h1>
+      <h1 style={appStyle}>First batch is the best batch</h1>
      <AboutUs />
         <div className="members-container">
           <Member name="Raju" city="Hyderabad"/>
