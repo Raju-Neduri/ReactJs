9c01daad9be1585904bea391913a74644911cfa8
Neduri Raju <rajneduri123@gmail.com>
2025-04-13 14:42:02 +0530

    values updated

--- ReactJs_Telugu/first_project/src/App.js
+++ ReactJs_Telugu/first_project/src/App.js
@@ -1,57 +1,60 @@
-
-import AboutUs from './AboutUs';
-import './App.css';
-import { Member } from './Member';
+import AboutUs from "./Components/About us/AboutUs";
+import "./App.css";
+import { Member } from "./Components/Member/Member";
 
 function App() {
+  const appStyle = {
+    backgroundColor: "#3A59D1",
+    textAlign: "center",
+    color: "white",
+  };
 
-const appStyle={backgroundColor:'#3A59D1', textAlign:"center", color:'white'}
-
-const people = [
-  { name: "Raju", city: "Jammikunta" },
-  { name: "Vaishu", city: "Houseboard Colony" },
-  { name: "Abhilash", city: "Valbhapur" },
-  { name: "Ram", city: "Hyderabad" },
-  { name: "Anu", city: "Karimnagar" },
-  { name: "Pooja", city: "Warangal" },
-  { name: "Surya", city: "Kazipet" },
-  { name: "Meghana", city: "Hanamkonda" },
-  { name: "Kiran", city: "Chityal" },
-  { name: "Bhavya", city: "Guntur" },
-  { name: "Vamsi", city: "Khammam" },
-  { name: "Sravani", city: "Nalgonda" },
-  { name: "Manoj", city: "Bhimavaram" },
-  { name: "Neha", city: "Vijayawada" },
-  { name: "Akash", city: "Rajahmundry" },
-  { name: "Harsha", city: "Nizamabad" },
-  { name: "Deepthi", city: "Adilabad" },
-  { name: "Sandeep", city: "Karimnagar" },
-  { name: "Lahari", city: "Siddipet" },
-  { name: "Rohit", city: "Mahabubabad" },
-  { name: "Divya", city: "Warangal" },
-  { name: "Tarun", city: "Yellandu" },
-  { name: "Keerthi", city: "Huzurabad" },
-  { name: "Venky", city: "Manthani" },
-  { name: "Madhu", city: "Kothagudem" },
-  { name: "Swapna", city: "Sircilla" },
-  { name: "Praveen", city: "Bhadrachalam" },
-  { name: "Lavanya", city: "Hanamkonda" },
-  { name: "Sushma", city: "Metpally" },
-  { name: "Rakesh", city: "Jagtial" }
-];
+  const people = [
+    { name: "Raju", city: "Jammikunta" },
+    { name: "Vaishu", city: "Housing board Colony" },
+    { name: "Abhilash", city: "Valbhapur" },
+    { name: "Ram", city: "Hyderabad" },
+    { name: "Anu", city: "Karimnagar" },
+    { name: "Pooja", city: "Warangal" },
+    { name: "Surya", city: "Kazipet" },
+    { name: "Meghana", city: "Hanamkonda" },
+    { name: "Kiran", city: "Chityal" },
+    { name: "Bhavya", city: "Guntur" },
+    { name: "Vamsi", city: "Khammam" },
+    { name: "Sravani", city: "Nalgonda" },
+    { name: "Manoj", city: "Bhimavaram" },
+    { name: "Neha", city: "Vijayawada" },
+    { name: "Akash", city: "Rajahmundry" },
+    { name: "Harsha", city: "Nizamabad" },
+    { name: "Deepthi", city: "Adilabad" },
+    { name: "Sandeep", city: "Karimnagar" },
+    { name: "Lahari", city: "Siddipet" },
+    { name: "Rohit", city: "Mahabubabad" },
+    { name: "Divya", city: "Warangal" },
+    { name: "Tarun", city: "Yellandu" },
+    { name: "Keerthi", city: "Huzurabad" },
+    { name: "Venky", city: "Manthani" },
+    { name: "Madhu", city: "Kothagudem" },
+    { name: "Swapna", city: "Sircilla" },
+    { name: "Praveen", city: "Bhadrachalam" },
+    { name: "Lavanya", city: "Hanamkonda" },
+    { name: "Sushma", city: "Metpally" },
+    { name: "Rakesh", city: "Jagtial" },
+  ];
 
   return (
-   
     <div>
       <h1 style={appStyle}>First batch is the best batch</h1>
-     <AboutUs />
-        <div className="members-container">
-          <Member name="Raju" city="Hyderabad"/>
+      <AboutUs />
+      <div className="members-container">
+        {people.map((person, index) => (
+          <Member key={index} name={person.name} city={person.city} />
+        ))}
+        {/* <Member name="Raju" city="Hyderabad"/>
           <Member name="Ram" city="jammikunta"/>
-          <Member name="vaishu" city="Hanamkonda" />
+          <Member name="vaishu" city="Hanamkonda" /> */}
+      </div>
     </div>
-    </div>
-  
   );
 }
 
--- ReactJs_Telugu/first_project/src/Components/Member/Member.js
+++ ReactJs_Telugu/first_project/src/Components/Member/Member.js
@@ -1,14 +1,13 @@
-
 import "./Member.css";
 export function Member(props) {
   console.log(props.city);
-  const memebersName=props.name;
-  const memebersCity=props.city;
+  const memebersName = props.name;
+  const memebersCity = props.city;
 
   return (
     <div className="memberCard">
       <h2>{memebersName}</h2>
-      <p>{memebersCity}</p>
+      <p className="member-sub-heading">{memebersCity}</p>
     </div>
   );
 }
