c7814663a0d402100f1b05fdfe23d3c7b9e62538
Neduri Raju <rajneduri123@gmail.com>
2025-06-12 12:57:17 +0530

    Congrats Card

---
+++ Nxtwave/Introduction to React JS/Congrats Card/index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="container">
+    <h1>Congratulations</h1>
+    <div className="inside-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        alt="Profile icon"
+      />
+      <h2>Kiran V</h2>
+      <p>Vishnu institute of Computer Education and Technology,Bhimavaram</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        alt="watch"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
