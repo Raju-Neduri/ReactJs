17f16a659e7652b5d851db6c828dc74e25a62271
Neduri Raju <rajneduri123@gmail.com>
2025-06-17 11:39:33 +0530

    using usestate

--- Nxtwave/4 Class Components and state/fruitscounter/src/components/FruitCounter/index.js
+++ Nxtwave/4 Class Components and state/fruitscounter/src/components/FruitCounter/index.js
@@ -1,61 +1,45 @@
-// Write your code here
-import { Component } from "react";
+import React, { useState } from "react";
 import "./index.css";
 
-class FruitsCounter extends Component {
-  state = {
-    mangoCount: 0,
-    bananaCount: 0,
-  };
+const FruitsCounter = () => {
+  const [mangoCount, setMangoCount] = useState(0);
+  const [bananaCount, setBananaCount] = useState(0);
 
-  eatMango = () => {
-    this.setState((prevState) => ({
-      mangoCount: prevState.mangoCount + 1,
-    }));
-  };
+  const incrementMango = () => setMangoCount((prev) => prev + 1);
+  const incrementBanana = () => setBananaCount((prev) => prev + 1);
 
-  eatBanana = () => {
-    this.setState((prevState) => ({
-      bananaCount: prevState.bananaCount + 1,
-    }));
-  };
-
-  render() {
-    const { mangoCount, bananaCount } = this.state;
-
-    return (
-      <div className="fruits-counter-bg">
-        <div className="counter-card">
-          <h1 className="heading">
-            Bob ate <span className="count">{mangoCount}</span> mangoes{" "}
-            <span className="count">{bananaCount}</span> bananas
-          </h1>
-          <div className="fruit-container">
-            <div className="fruit">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/mango-img.png"
-                className="fruit-img"
-                alt="mango"
-              />
-              <button type="button" className="button" onClick={this.eatMango}>
-                Eat Mango
-              </button>
-            </div>
-            <div className="fruit">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/banana-img.png"
-                className="fruit-img"
-                alt="banana"
-              />
-              <button type="button" className="button" onClick={this.eatBanana}>
-                Eat Banana
-              </button>
-            </div>
+  return (
+    <div className="fruits-counter-bg">
+      <div className="counter-card">
+        <h1 className="heading">
+          Bob ate <span className="count">{mangoCount}</span> mangoes{" "}
+          <span className="count">{bananaCount}</span> bananas
+        </h1>
+        <div className="fruit-container">
+          <div className="fruit">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/mango-img.png"
+              alt="mango"
+              className="fruit-img"
+            />
+            <button className="button" onClick={incrementMango}>
+              Eat Mango
+            </button>
+          </div>
+          <div className="fruit">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/banana-img.png"
+              alt="banana"
+              className="fruit-img"
+            />
+            <button className="button" onClick={incrementBanana}>
+              Eat Banana
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default FruitsCounter;
