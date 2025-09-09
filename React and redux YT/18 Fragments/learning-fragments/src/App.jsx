7af7281161bf2c96533e96eb2c27065db09ddd92
Neduri Raju <rajneduri123@gmail.com>
2025-09-09 17:54:17 +0530

    feat: implement App component with food items list and styling

--- React and redux YT/18 Fragments/learning-fragments/src/App.jsx
+++ React and redux YT/18 Fragments/learning-fragments/src/App.jsx
@@ -1,10 +1,27 @@
+import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const foodItems = [
+    "Fresh Fruits",
+    "Green Vegetables",
+    "Dry Fruits",
+    "Whole Grains",
+    "Dairy Products",
+  ];
   return (
     <>
-      <center>Hi</center>
+      <div className="App">
+        <h1>Healthy Food</h1>
+        <ul className="list-group">
+          {foodItems.map((item, index) => (
+            <li key={index} className="list-group-item">
+              {index + 1}. {item}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
