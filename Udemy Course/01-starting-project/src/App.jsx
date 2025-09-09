6ad6d979e01444b6036089f186d40bf5b0df87b6
Neduri Raju <rajneduri123@gmail.com>
2025-09-09 18:35:36 +0530

    feat: refactor Header component and move styles to separate CSS file

--- Udemy Course/01-starting-project/src/App.jsx
+++ Udemy Course/01-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import CoreConceptsSection from "./components/CoreConceptsSection.jsx";
 
 function App() {
