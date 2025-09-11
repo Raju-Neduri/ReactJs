5fc2723e562d9333503130d083d9f371b643857d
Neduri Raju <rajneduri123@gmail.com>
2025-09-11 17:28:53 +0530

    feat: add Item component with styles for food items

--- React and redux YT/18 Fragments/learning-fragments/src/components/Item.jsx
+++ React and redux YT/18 Fragments/learning-fragments/src/components/Item.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./Item.module.css";
 
 const Item = ({ foodItem, index }) => {
   return (
     <>
-      <li className="list-group-item">
+      <li className={`list-group-item ${styles.foodItem}`}>
         {index + 1}. {foodItem}
       </li>
     </>
