1d2fce79f21f988fd38fe9335255c8244526673f
Neduri Raju <rajneduri123@gmail.com>
2025-09-22 21:02:44 +0530

    Lifting State Up [Core Concept]

--- Udemy Course/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ Udemy Course/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,39 +1,38 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard() {
+
+export default function GameBoard({ onSelectSqaure, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      prevGameBoard[rowIndex][colIndex] = "X";
+      const updatedBoard = prevGameBoard.map((innerArray) => [...innerArray]);
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+    onSelectSqaure();
   }
+
   return (
-    <>
-      <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => (
-          <li key={rowIndex}>
-            <ol>
-              {row.map((playerSymbol, colIndex) => (
-                <li key={colIndex}>
-                  <button
-                    onClick={() => handleSelectSquare(rowIndex, colIndex)}
-                  >
-                    {playerSymbol}
-                  </button>
-                </li>
-              ))}
-            </ol>
-          </li>
-        ))}
-      </ol>
-    </>
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
   );
 }
