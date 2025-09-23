f39ec2d78c8905e597da4abc092a3df9a6ccf566
Neduri Raju <rajneduri123@gmail.com>
2025-09-23 20:16:04 +0530

    Children

--- React and redux YT/18 Fragments/learning-fragments/src/App.jsx
+++ React and redux YT/18 Fragments/learning-fragments/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 function App() {
   const foodItems = [
@@ -15,11 +16,23 @@ function App() {
 
   return (
     <>
-      <div className="App">
-        <h1>Healthy Food</h1>
-        <ErrorMessage items={foodItems} />
-        <FoodItems items={foodItems} />
-      </div>
+      <Container>
+        <div className="App">
+          <h1>Healthy Food</h1>
+          <ErrorMessage items={foodItems} />
+          <FoodItems items={foodItems} />
+        </div>
+      </Container>
+
+      <Container>
+        <h1>this is new container</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla illum
+          laborum esse iusto molestias qui facilis quisquam porro, atque veniam
+          voluptatum, accusantium officia laudantium necessitatibus labore neque
+          distinctio doloremque magnam.
+        </p>
+      </Container>
     </>
   );
 }
